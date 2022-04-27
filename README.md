fc4acc32cfc63880069940baf80a811224fffefd
Benoit Richert <github@natura-rerum.com>
2022-04-27 23:28:46 +0200

    Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Keystone Utils
+
+## History [Beta]
+
+### Usage
+
+- Add History schema to your lists
+- Add the `afterOperationSaveHistory` hook to any object you want to historize
+
+### Example
+
+TBD, see testSchema/historized
--- schema.ts
+++ schema.ts
@@ -23,71 +23,11 @@ export const lists: Lists = {
         isFilterable: true,
       }),
       password: password({ validation: { isRequired: true } }),
-      posts: relationship({ ref: "Post.author", many: true }),
     },
     ui: {
       listView: {
-        initialColumns: ["name", "posts"],
+        initialColumns: ["name"],
       },
-    },
-  }),
-  Post: list({
-    fields: {
-      title: text(),
-      status: select({
-        options: [
-          { label: "Published", value: "published" },
-          { label: "Draft", value: "draft" },
-        ],
-        defaultValue: "draft",
-        ui: {
-          displayMode: "segmented-control",
-        },
-      }),
-      content: document({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
-        ],
-        links: true,
-        dividers: true,
-      }),
-      publishDate: timestamp(),
-      author: relationship({
-        ref: "User.posts",
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name", "email"],
-          inlineEdit: { fields: ["name", "email"] },
-          linkToItem: true,
-          inlineCreate: { fields: ["name", "email"] },
-        },
-      }),
-      tags: relationship({
-        ref: "Tag.posts",
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name"],
-          inlineEdit: { fields: ["name"] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ["name"] },
-        },
-        many: true,
-      }),
-    },
-  }),
-  Tag: list({
-    ui: {
-      isHidden: true,
-    },
-    fields: {
-      name: text(),
-      posts: relationship({ ref: "Post.tags", many: true }),
     },
   }),
 }
