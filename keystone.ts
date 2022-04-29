9a0199a4ade8e77db824f05f8cb0773156f8b557
Benoit Richert <github@natura-rerum.com>
2022-04-29 11:19:50 +0200

    CI, clean

--- keystone.ts
+++ keystone.ts
@@ -5,7 +5,6 @@ import { lists } from "./schema"
 const url = process.env.DATABASE_URL || "file:./keystone.db"
 const port = parseInt(process.env.PORT || "") || 3002
 
-console.log("url", url)
 export default config({
   db: {
     provider: "sqlite",
