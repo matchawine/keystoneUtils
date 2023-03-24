1d0dbf4e9ee40fb5173fac2aade2d86e029c3df6
AhmedBG <ahmed.ben.ghozzia@gmail.com>
2023-03-24 17:13:56 +0100

    apply review

--- src/url/index.ts
+++ src/url/index.ts
@@ -2,6 +2,7 @@ import { BaseListTypeInfo, FieldTypeFunc } from "@keystone-6/core/types"
 import { TextFieldConfig } from "@keystone-6/core/dist/declarations/src/fields/types/text"
 import { text } from "@keystone-6/core/fields"
 
+// https://stackoverflow.com/a/3809435
 const urlRegex =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
 
